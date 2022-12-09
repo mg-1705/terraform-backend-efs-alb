a6b6adf6cdbbe32baf9ae5ef43091ebfc0edca61
mg-1705 <84842366+mg-1705@users.noreply.github.com>
2022-12-09 15:46:40 +0530

    Update backup.tf

--- Terraform-2/Modular/backup.tf
+++ Terraform-2/Modular/backup.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "madhuryt-17839backend"
     key = "terraform.tfstate"
     region = "us-east-2"
+    access_key="AKIAQTO6MJDC3Z4KYCNS"
+    secret_key="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"
   }
 }
